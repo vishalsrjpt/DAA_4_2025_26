530c5bf51e8809915a8be75614c2af48e9f5d54f
Vishal Kumar Singh <codewithvishaldgp@gmail.com>
2026-04-30 12:35:05 +0530

    Create Set-2_Q1.cpp

---
+++ Practical_Lab_EST/Set-2_Q1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int lengthOfLIS(vector<int>& nums) {
+    vector<int> tails;
+    for (int x : nums) {
+        auto it = lower_bound(tails.begin(), tails.end(), x);
+        if (it == tails.end())
+            tails.push_back(x);
+        else
+            *it = x;
+    }
+    return tails.size();
+}
+
+int main() {
+    vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};
+    int result = lengthOfLIS(nums);
+    return 0;
+}
